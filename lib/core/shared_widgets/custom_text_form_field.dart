d49900775d4f6a7e7a51ede1aa8f8e829b5d84cf
Omar Gamal <omarthefirst6206@gmail.com>
2025-09-12 22:50:27 +0300

    Add Firebase Firestore integration and update UI components for chat functionality

--- lib/core/shared_widgets/custom_text_form_field.dart
+++ lib/core/shared_widgets/custom_text_form_field.dart
@@ -1,4 +1,3 @@
-import 'package:chat_app/core/constants/validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
--- lib/features/chat/presentation/views/widgets/chat_view_body.dart
+++ lib/features/chat/presentation/views/widgets/chat_view_body.dart
@@ -1,21 +1,57 @@
 import 'package:chat_app/core/constants/app_colors.dart';
 import 'package:chat_app/features/chat/presentation/views/widgets/chat_bubble.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ChatViewBody extends StatelessWidget {
-  const ChatViewBody({super.key});
+  ChatViewBody({super.key});
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  CollectionReference messages = FirebaseFirestore.instance.collection(
+    'messages',
+  );
+  final TextEditingController messageController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return ChatBubble(
-          color: AppColors.primaryColor,
-          text:
-              'Chat Messages will be displayed here. This is message number ${index + 1}.',
-        );
-      },
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ChatBubble(
+                color: AppColors.primaryColor,
+                text:
+                    'Chat Messages will be displayed here. This is message number ${index + 1}.',
+              );
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: messageController,
+            onSubmitted: (data) {
+              messages.add({"message": data});
+              messageController.clear();
+            },
+            decoration: InputDecoration(
+              hintText: 'Type your message...',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.r),
+                borderSide: const BorderSide(color: AppColors.primaryColor),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.r),
+                borderSide: const BorderSide(color: Colors.black),
+              ),
+              suffixIcon: Icon(Icons.send, color: AppColors.primaryColor),
+              // Handle send button press
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
