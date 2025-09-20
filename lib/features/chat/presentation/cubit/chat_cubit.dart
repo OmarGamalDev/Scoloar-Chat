5be74e27dab84fba924438cf43f60be1c9d13d1f
Omar Gamal <omarthefirst6206@gmail.com>
2025-09-20 14:29:30 +0300

    use cubit to Implement sign-in functionality with loading and success&failure states

--- lib/features/chat/presentation/cubit/chat_cubit.dart
+++ lib/features/chat/presentation/cubit/chat_cubit.dart
@@ -1,8 +1,21 @@
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
   ChatCubit() : super(ChatInitial());
+
+  Future<void> signin({required String email, required String password}) async {
+    emit(ChatLoading());
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(ChatSuccess());
+    } on Exception catch (e) {
+      emit(ChatFailure());
+    }
+  }
 }
--- lib/features/chat/presentation/cubit/chat_state.dart
+++ lib/features/chat/presentation/cubit/chat_state.dart
@@ -3,3 +3,6 @@ part of 'chat_cubit.dart';
 abstract class ChatState {}
 
 class ChatInitial extends ChatState {}
+class ChatLoading extends ChatState {}
+class ChatSuccess extends ChatState {}
+class ChatFailure extends ChatState {}
